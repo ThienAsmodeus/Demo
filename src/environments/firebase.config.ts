b74419fc7b9c9ea8020a5afa92015185f1eec003
ThienAsmodeus <48200727+ThienAsmodeus@users.noreply.github.com>
2019-03-04 15:35:29 +0700

    first commit

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,14 @@
+
+
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDhsZXnvUplhHGeK1AR-hYGfeG-TBv0X4o",
+    authDomain: "onlineshop-e9dd5.firebaseapp.com",
+    databaseURL: "https://onlineshop-e9dd5.firebaseio.com",
+    projectId: "onlineshop-e9dd5",
+    storageBucket: "onlineshop-e9dd5.appspot.com",
+    messagingSenderId: "1089780098369"
+};
+
+
